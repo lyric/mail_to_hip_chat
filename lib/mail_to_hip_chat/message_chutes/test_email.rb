c3775430017d895aae81ea5ca9e935a1218dbe57
Lyric Hartley <lhartley@salesforce.com>
2013-07-31 13:52:27 -0700

    remove from old file, bump version

--- lib/mail_to_hip_chat/message_chutes/test_email.rb
+++ lib/mail_to_hip_chat/message_chutes/test_email.rb
@@ -21,20 +21,5 @@ module MailToHipChat
       
     end
 
-    class JenkinsEmail
-      include MessageChute
-      
-      def initialize(opts)
-        initialize_hipchat_opts(opts)
-      end
-      
-      def call(params)
-        #return false unless params["subject"] =~ /testing setup/i
-        message = Mustache.render("Message:<br />{{message}}", :message => params["plain"])
-        message_rooms("Jenkins", message)
-        true
-      end
-      
-    end
   end
 end
